2d0e2128bf77385ee55f5a972eaf245195214ec0
JoseVarela8 <45233198+JoseVarela8@users.noreply.github.com>
2024-09-09 21:01:05 -0300

    Added gRPC

--- server.js
+++ server.js
@@ -1,15 +1,67 @@
-import express from 'express'
-import swaggerDocs from "./swagger.js";
+import grpc from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader';
 
-import messageRouter from './src/v1/routers/messageRouter.js'
+// Cargar el archivo .proto
+const packageDef = protoLoader.loadSync("messaging.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
 
-const app = express();
-const port = 3000;
+// Asignar el paquete correcto desde el archivo .proto
+const MessagePackage = grpcObject.andisbank.mensajeria;
 
-app.use(express.json());
-app.use('/api/v1', messageRouter);
+// Almacén de mensajes (simulación)
+const Messages = [];
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
-swaggerDocs(app, port);
+// Implementación de GetMessage
+function GetMessage(call, callback) {
+    const messageId = call.request.id;
+    const message = Messages.find(msg => msg.id === messageId);
+    if (message) {
+        callback(null, message);
+    } else {
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: "Message not found"
+        });
+    }
+}
+
+// Implementación de SendMessage
+function SendMessage(call, callback) {
+    const data = call.request;
+
+    // Agregar un nuevo mensaje con un id único
+    const newMessageData = { ...data, id: Messages.length + 1 };
+    Messages.push(newMessageData);
+  
+    // Devolver el nuevo mensaje como respuesta
+    callback(null, newMessageData);
+}
+
+// Implementación de DeleteMessage
+function DeleteMessage(call, callback) {
+    const messageId = call.request.id;
+    const index = Messages.findIndex(msg => msg.id === messageId);
+
+    if (index !== -1) {
+        Messages.splice(index, 1); // Eliminar el mensaje
+        callback(null, { message: 'Message deleted successfully' });
+    } else {
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: "Message not found"
+        });
+    }
+}
+
+// Crear el servidor gRPC y añadir el servicio
+const server = new grpc.Server();
+server.addService(MessagePackage.Message.service, { 
+    GetMessage,
+    SendMessage,
+    DeleteMessage,
+});
+
+// Iniciar el servidor en el puerto 4000, `start()` ya no es necesario
+server.bindAsync("0.0.0.0:4000", grpc.ServerCredentials.createInsecure(), () => {
+    console.log("gRPC Server running at http://0.0.0.0:4000");
+});
