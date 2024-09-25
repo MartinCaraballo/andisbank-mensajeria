0bec2e5e8385ecef336ff950af68f669d186ff8a
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2024-09-25 19:52:13 -0300

    ratelimiter window

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ import messageRouter from './src/v1/routers/messageRouter.js'
 
 const app = express();
 const port = 3000;
+const rateLimitMiddleware = require('./src/v1/config/ratelimiter.js');
 
 app.use(express.json());
 app.use('/api/v1', messageRouter);
+app.use(rateLimitMiddleware);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
